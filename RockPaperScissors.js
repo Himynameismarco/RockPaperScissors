f502676fae4173e23ad80f4fa8faee116500dab6
Himynameismarco <marcozander94@web.de>
2022-11-24 11:15:00 +0100

    Added 5 rounds till end logic

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,22 +1,35 @@
 console.log("Hello World!");
 
-function getComputerChoice() {
-  let float = Math.random();
-  let computerChoice;
-  if (float <= 0.33) {
-    computerChoice = "Schere";
+function game() {
+  let playerWonCount = 0;
+  let computerWonCount = 0;
+  for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = prompt("Schere, Stein oder Papier?");
+    let endGameMessage = playRound(computerChoice, playerChoice);
+    if (endGameMessage.startsWith("Ihr habt")) {
+      //nothing
+    }
+    if (endGameMessage.startsWith("Du hast")) {
+      computerWonCount++;
+    }
+    if (endGameMessage.startsWith("GEWONNEN")) {
+      playerWonCount++;
+    }
+    if ((i === 4 && playerWonCount === computerWonCount) || endGameMessage.startsWith("Falscher")) {
+      i--;
+    }
+    console.log("Game count: " + (i + 1));
+    console.log("You won " + playerWonCount + " matches so far, "
+        + "the computer won " + computerWonCount + " matches so far.");
   }
-  if (0.66 >= float > 0.33) {
-    computerChoice = "Stein";
+  if (playerWonCount > computerWonCount) {
+    alert("GAME WON!");
+  } else {
+    alert("YOU LOST.")
   }
-  if (float > 0.66) {
-    computerChoice = "Papier";
-  }
-  return computerChoice;
 }
 
-let computerChoice = getComputerChoice();
-let playerChoice = prompt("Schere, Stein oder Papier?");
 
 function playRound(computerChoice, playerChoice) {
   console.log("Computer Choice: " + getComputerChoice());
@@ -36,9 +49,9 @@ function playRound(computerChoice, playerChoice) {
       if ((playerChoice === schere && computerChoice === stein)
           || (playerChoice === stein && computerChoice === papier)
           || (playerChoice === papier && computerChoice === schere)) {
-        endGameMessage = "Du hast verloren! Der Computer hatte: " + computerChoice;
+        endGameMessage = "Du hast die Runde verloren! Der Computer hatte: " + computerChoice;
       } else {
-        endGameMessage = "GEWONNEN!!!! Der Computer hatte: " + computerChoice;
+        endGameMessage = "GEWONNEN!!!! (zumindest diese Runde) Der Computer hatte: " + computerChoice;
       }
     }
   } else {
@@ -51,5 +64,19 @@ function playRound(computerChoice, playerChoice) {
   return endGameMessage;
 }
 
-playRound(computerChoice, playerChoice);
+function getComputerChoice() {
+  let float = Math.random();
+  let computerChoice;
+  if (float <= 0.33) {
+    computerChoice = "Schere";
+  }
+  if (0.66 >= float > 0.33) {
+    computerChoice = "Stein";
+  }
+  if (float > 0.66) {
+    computerChoice = "Papier";
+  }
+  return computerChoice;
+}
 
+game();
